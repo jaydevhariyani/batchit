a8b572dcc880047469450cf5697e28d181f8d294
jaydevhariyani <jaydevhariyani@gmail.com>
2026-09-22 16:56:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -311,7 +311,7 @@ sendBtn.addEventListener('click', async () => {
                             "Authorization": `Bearer ${GROQ_API_KEY.trim()}`
                         },
                         body: JSON.stringify({
-                            model: "llama-3.3-70b-versatile",
+                            model: "llama3-8b-8192",
                             messages: [{ role: "user", content: promptText }]
                         })
                     });
